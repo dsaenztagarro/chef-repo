c2a65667ed652c7ee4dd22ec981bb6a250f58477
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2016-01-04 23:24:39 +0100

    Adds uploaded cookbooks

---
+++ .chef/chef-shell.rb
@@ -0,0 +1,4 @@
+current_dir = File.dirname(__FILE__)
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+chef_server_url          "https://mychefserver.es/organizations/digitalocean"
